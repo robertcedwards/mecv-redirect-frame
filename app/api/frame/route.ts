f9394a12f22136fcf707622d5dd16e94f3d1ffc4
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-08-20 10:49:12 -0500

    Initial commit

---
+++ app/api/frame/route.ts
@@ -0,0 +1,63 @@
+import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
+import { NextRequest, NextResponse } from 'next/server';
+import { NEXT_PUBLIC_URL } from '../../config';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+
+  if (!isValid) {
+    return new NextResponse('Message not valid', { status: 500 });
+  }
+
+  const text = message.input || '';
+  let state = {
+    page: 0,
+  };
+  try {
+    state = JSON.parse(decodeURIComponent(message.state?.serialized));
+  } catch (e) {
+    console.error(e);
+  }
+
+  /**
+   * Redirect to a URL with the user's input appended
+   */
+  if (message?.button === 3) {
+    const redirectUrl = `https://mecvapp.netlify.app/user/${encodeURIComponent(text)}`;
+    return NextResponse.redirect(redirectUrl, { status: 302 });
+  }
+
+  return new NextResponse(
+    getFrameHtmlResponse({
+      buttons: [
+        {
+          label: `State: ${state?.page || 0}`,
+        },
+        {
+          action: 'link',
+          label: 'OnchainKit',
+          target: 'https://onchainkit.xyz',
+        },
+        {
+          action: 'post_redirect',
+          label: 'Dog pictures',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/park-1.png`,
+      },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+      state: {
+        page: state?.page + 1,
+        time: new Date().toISOString(),
+      },
+    }),
+  );
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
